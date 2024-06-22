b62f23c3f7eaae71d89a8efc9168c065a3b382ad
Anskrevy <95379591+Anskrevy@users.noreply.github.com>
2024-06-21 21:16:00 -0700

    Fix for breaking changes in webtorrent 5.1.X

--- webtorrent-wrap.sh
+++ webtorrent-wrap.sh
@@ -23,12 +23,24 @@ url=$(tail -f "$webtorrent_output_file" \
 		  | awk '/Server running at: ?/ {gsub(/Server running at: ?/, ""); print $1; exit}')
 
 base_url=$(echo "$url" | grep --extended-regexp --only-matching \
-							  'http://localhost:[0-9]+/')
-# print json of files
-xidel --silent --extract "//a/@href" "$base_url" |
-	jq --null-input --raw-input "
+							  'http://localhost:[0-9]+')
+webtorrent_hash=$(echo "$url" | grep --extended-regexp --only-matching \
+							  'webtorrent/[0-9a-f]+')
+
+# Get json of files
+webtorrent_results=$(xidel --silent --extract "//a/@href" "$base_url/$webtorrent_hash" |
+  jq --null-input --raw-input "
 {
     pid: $pid,
-    files: [inputs |  select(length>0)] | map({title: . | sub(\"[0-9]+/\"; \"\"), url: (\"$base_url\" + .)})
+    files: [inputs |  select(length>0)] | map({title: . | sub(\"/$webtorrent_hash/\"; \"\"), url: (\"$base_url\" + .)})
 }
-"
+")
+
+# Uncomment for debugging info
+# echo "$webtorrent_results" > ~/webtorrent-wrap.log
+# echo "URL             - $url" >> ~/webtorrent-wrap.log
+# echo "WEBTORRENT_HASH - $webtorrent_hash" >> ~/webtorrent-wrap.log
+# echo "BASE_URL        - $base_url" >> ~/webtorrent-wrap.log
+
+# Print results
+echo "$webtorrent_results"
